3869a84971e316d0f65d621d1aca7eda8ded3ad2
fernando-bertolo <fernando.bertolo.jr123@gmail.com>
2023-10-12 00:28:38 -0300

    Realizando tela de login e autenticação

--- frontend/src/Components/Login/index.jsx
+++ frontend/src/Components/Login/index.jsx
@@ -68,7 +68,7 @@ function Login(){
 
                 </FormLogin >
                 <DivBotao>
-                    <Botao type="submit" onClick={(e) => handleLogin(e)}>Logi</Botao>
+                    <Botao type="submit" onClick={(e) => handleLogin(e)}>Login</Botao>
                 </DivBotao>
             </SectionLogin>
             <p >{mensagemError}</p>
