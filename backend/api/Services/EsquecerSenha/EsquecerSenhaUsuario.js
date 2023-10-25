728a9c2b2ffe75d82995bb21a6c808dc8587a17b
fernando-bertolo <fernando.bertolo.jr123@gmail.com>
2023-10-25 00:03:21 -0300

    Criação do token de email e expiração

--- backend/api/Services/EsquecerSenha/EsquecerSenhaUsuario.js
+++ backend/api/Services/EsquecerSenha/EsquecerSenhaUsuario.js
@@ -2,31 +2,43 @@ const express = require("express");
 const alterarSenha = express();
 const tabelaUsuario = require("../../../Database/Tabelas/Usuarios/usuarios.js");
 const crypto = require("crypto");
+const { where } = require("sequelize");
 
 
 alterarSenha.post("/esquecer-senha", async (request, response) => {
     const {email} = request.body;
     try{
 
-        const validaEmail = await tabelaUsuario.findOne({
+        const usuario = await tabelaUsuario.findOne({
             where: {
                 email: email
             }
         })
     
-        if(!validaEmail){
-            return response.status(401).json({message: "E-mail não encontrado, por favor digite um e-mail válido!!"})
-        }else{
-            return response.status(200).json({message: "Sucesso"})
+        if(!usuario){
+            return response.status(400).json({message: "E-mail não encontrado, por favor digite um e-mail válido!!"})
+            
         }
+
 
         const tokenEmail = crypto.randomBytes(20).toString("hex"); // Gera token do e-mail de 20 caracteres
 
         const horaAtual = new Date();
-        horaAtual.setHours(horaAtual.getHours() + 1)
+        horaAtual.setHours(horaAtual.getHours() + 1);
 
+        await tabelaUsuario.update(
+            {
+                '$set': {
+                    senhaResetaToken: tokenEmail,
+                    tokenExpiracao: horaAtual,
+                }
+            },
+            {where: {id: request.params.id}}    
+        );
+        console.log(tokenEmail, horaAtual)
 
-    } catch(err){
+    } catch(error){
+        console.log(error)
         response.status(400).send({error: "ERRO"})
     }
 });
--- backend/api/server.js
+++ backend/api/server.js
@@ -8,10 +8,7 @@ const rotas = require("./Controllers/index.js");
 const port = 3010;
 app.listen(port);
 
-app.get("/", (req,res) => {
-    console.log("API Funcionando na porta 3010");
-    res.send("API funcionando na porta 3010")
-});
+
 
 app.use(rotas);
 
