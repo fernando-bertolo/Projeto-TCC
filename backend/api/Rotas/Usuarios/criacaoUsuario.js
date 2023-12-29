3246bd8b496274646cba85dd9147c071da17bbf3
fernando-bertolo <fernando.bertolo.jr123@gmail.com>
2023-12-29 17:32:33 -0300

    Ajustando criação de usuário pelo modal de usuário

--- backend/api/Rotas/Usuarios/criacaoUsuario.js
+++ backend/api/Rotas/Usuarios/criacaoUsuario.js
@@ -4,20 +4,67 @@ const tabelaUsuarios = require("../../../Database/Tabelas/Usuarios/usuarios.js")
 const bcrypt = require("bcrypt");
 const saltRounds = 10; // saltRounds é a quantidade de vezes que o algoritmo é executado
 
-criacaoUsuario.post("/criacao-usuario", (request, response) => {
+criacaoUsuario.post("/criacao-usuario", async (request, response) => {
+
+    const {nome, email, usuario, senha, confirmaSenha} = request.body;
+    
     try{
+
+        const validaUsuario = await tabelaUsuarios.findOne({
+            where: {
+                usuario: usuario, //Consulta no banco se já existe um usuário igual o passado na requisição
+            }
+        })
+
+        const validaEmail = await tabelaUsuarios.findOne({
+            where: {
+                email: email //Consulta no banco se já existe um email igual o passado na requisição
+            }
+        })
+
+        response.send({
+            usuario: usuario,
+            email: email
+        })
+
+        // Validação de usuário e validação de e-mail  
+        if(validaUsuario){
+            return response.status(400).json({Error: "Usuário já existe"})
+        } else if(validaEmail){
+            return response.status(400).json({Error: "E-mail já cadastrado no sistema"})
+        }
+
+
+        if(senha === "" && confirmaSenha === ""){
+            return response.status(400).json({Error: "Os campos das senhas devem ser preenchidos"})
+        } else if (senha !== confirmaSenha){
+            return response.status(400).json({Error: "As senhas estão diferentes"})
+        } else if(senha.length < 5 && confirmaSenha.length < 5){
+            return response.status(400).json({Error: "A senha deverá ter no mínimo 5 caracteres"})
+        } else{
+
+        //Realizando o hash da senha
         const hashSenha = bcrypt.hashSync(request.body.senha, saltRounds);
 
-        //Objeto usuario com a senha hasheada
-        const usuario = {
-            ...request.body, // o "...request.body" estamos pegando o que já tem na requisição e alterando somente um valor, no caso senha passado no objeto
+        tabelaUsuarios.create({
+            nome: nome,
+            email: email,
+            usuario: usuario,
             senha: hashSenha,
+        });
+
+        return response.status(200).json({message: "Usuario criado com sucesso"});
+
         }
-        tabelaUsuarios.create(usuario);
-        response.send("Usuario criado com sucesso");
+
     } catch(error){
         console.log(error);
     }
+})
+
+
+criacaoUsuario.get("/criacao-usuario",  async (request, response) => {
+
 })
 
 module.exports = criacaoUsuario;
--- frontend/src/Components/Modals/Usuarios/modalUsuario.jsx
+++ frontend/src/Components/Modals/Usuarios/modalUsuario.jsx
@@ -1,3 +1,8 @@
+import axios from "axios";
+import { useState } from "react";
+import {ToastContainer, toast} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
 import {
     SectionMainContent,
     MainContentbackground,
@@ -15,9 +20,119 @@ import {
     BotaoAdicionar
 } from "./style-modalUsuario.jsx"
 
+
+
 function ModalUser({isOpen, setModalOpen}) {
 
+    const [inputNome, setInputNome] = useState();
+    const [inputEmail, setInputEmail] = useState();
+    const [inputUsuario, setInputUsuario] = useState();
+    const [inputSenha, setInputSenha] = useState();
+    const [inputConfirmaSenha, setInputConfirmaSenha] = useState();
+
+    // Função de delay
+    function delay(n) {
+        return new Promise(function(resolve) {
+            setTimeout(resolve, n * 1000);
+        });
+    }
+
     if(isOpen){
+
+        const criacaoUsuario = async (event) => {
+            try {
+                event.preventDefault();            
+                
+                if(inputNome === ""){
+                    toast.warn("O campo de nome deve ser preenchido", {
+                        position: "bottom-right",
+                        autoClose: 2500,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "light",
+                        });
+                } else if(inputSenha === "" && inputConfirmaSenha === ""){
+                    toast.warn("O campo de senha deve ser preenchido", {
+                        position: "bottom-right",
+                        autoClose: 2500,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "light",
+                        });
+                } else if(inputSenha !== inputConfirmaSenha){
+                    toast.warn("Senhas diferentes", {
+                        position: "bottom-right",
+                        autoClose: 2500,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "light",
+                        });
+                } else if(inputSenha.length < 5 && inputConfirmaSenha.length < 5){
+                    toast.warn("A senha deverá ter no mínimo 5 caracteres", {
+                        position: "bottom-right",
+                        autoClose: 2500,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "light",
+                        });
+                } else {
+                    // Enviando os dados para a rota /criacao-usuario
+                    const response = await axios.post("http://localhost:3010/criacao-usuario", {
+                        nome: inputNome,
+                        email: inputEmail,
+                        usuario: inputUsuario,
+                        senha: inputSenha,
+                        confirmaSenha: inputConfirmaSenha,
+                    })    
+
+                    if(inputEmail === response.data.email){
+                        toast.warn("o E-mail já esta cadastrado no sistema", {
+                            position: "bottom-right",
+                            autoClose: 2500,
+                            hideProgressBar: false,
+                            closeOnClick: true,
+                            pauseOnHover: true,
+                            draggable: true,
+                            progress: undefined,
+                            theme: "light",
+                            });
+                    } else{
+                        // Mensagem de sucesso
+                        toast.success("Usuario criado com sucesso", {
+                            position: "bottom-right",
+                            autoClose: 2000,
+                            hideProgressBar: false,
+                            closeOnClick: true,
+                            pauseOnHover: true,
+                            draggable: true,
+                            progress: undefined,
+                            theme: "light",
+                            });
+                        await delay(4) // aguarda 4 segundos
+                        setModalOpen(false); // fecha o modal
+                        }
+                }
+                
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+
+
+
         return(
             <>
                 <MainContentbackground>
@@ -31,35 +146,70 @@ function ModalUser({isOpen, setModalOpen}) {
                                 <DivNomeInput>
                                     <NomeInput>Nome</NomeInput>
                                 </DivNomeInput>
-                                <Input type="text" name="nome" id="nome" placeholder="Digite o nome"/>
+                                <Input 
+                                type="text" 
+                                name="nome" 
+                                id="nome" 
+                                placeholder="Digite o nome"
+                                required
+                                onChange={(event) => {setInputNome(event.target.value)}}
+                                />
                             </DivInputs>
 
                             <DivInputs>
                                 <DivNomeInput>
                                     <NomeInput>E-mail</NomeInput>
                                 </DivNomeInput>
-                                <Input type="email" name="email" id="email" placeholder="Digite o e-mail"/>
+                                <Input 
+                                type="email" 
+                                name="email" 
+                                id="email" 
+                                placeholder="Digite o e-mail"
+                                required
+                                onChange={(event) => {setInputEmail(event.target.value)}}
+                                />
                             </DivInputs>
 
                             <DivInputs>
                                 <DivNomeInput>
                                     <NomeInput>Usuário</NomeInput>
                                 </DivNomeInput>
-                                <Input type="text" name="usuario" id="usuario" placeholder="Digite o usuário"/>
+                                <Input 
+                                type="text" 
+                                name="usuario" 
+                                id="usuario" 
+                                placeholder="Digite o usuário"
+                                required
+                                onChange={(event) => {setInputUsuario(event.target.value)}}
+                                />
                             </DivInputs>
 
                             <DivInputs>
                                 <DivNomeInput>
                                     <NomeInput>Senha</NomeInput>
                                 </DivNomeInput>
-                                <Input type="password" name="senha" id="senha" placeholder="Digite a senha"/>
+                                <Input 
+                                type="password" 
+                                name="senha" 
+                                id="senha" 
+                                placeholder="Digite a senha"
+                                required
+                                onChange={(event) => {setInputSenha(event.target.value)}}
+                                />
                             </DivInputs>
 
                             <DivInputs>
                                 <DivNomeInput>
                                     <NomeInput>Confirme sua Senha</NomeInput>
                                 </DivNomeInput>
-                                <Input type="password" name="confirmaSenha" id="confirmaSenha" placeholder="Digite a senha"/>
+                                <Input 
+                                type="password" 
+                                name="confirmaSenha" 
+                                id="confirmaSenha" 
+                                required
+                                placeholder="Digite a senha"
+                                onChange={(event) => {setInputConfirmaSenha(event.target.value)}}
+                                />
                             </DivInputs>
                             
                             <DivInputs>
@@ -74,9 +224,10 @@ function ModalUser({isOpen, setModalOpen}) {
                         </FormCadastro>
                         <DivBotoes>
                             <BotaoCancelar onClick={() => {setModalOpen(false)}}>CANCELAR</BotaoCancelar>
-                            <BotaoAdicionar>ADICIONAR</BotaoAdicionar>
+                            <BotaoAdicionar type="submit" onClick={(event) => criacaoUsuario(event)}>ADICIONAR</BotaoAdicionar>
                         </DivBotoes>
                     </SectionMainContent>
+                    <ToastContainer/>
                 </MainContentbackground>
             </>
         )
