d6da19d957505f5cf07f78ac75650ae08f9dbf8e
fernando-bertolo <fernando.bertolo.jr123@gmail.com>
2024-03-30 14:51:53 -0300

    feat: Finalizando tela de Marcas

--- frontend/src/Pages/Cadastros/Modelos/index.jsx
+++ frontend/src/Pages/Cadastros/Modelos/index.jsx
@@ -4,17 +4,43 @@ import {
   Body,
   DivContentCadastros,
 } from "../../../Components/BodyPages/style.jsx";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 import NavegacaoCadastro from "../../../Components/NavegacaoCadastros/index.jsx";
 
 function Modelos() {
+  const [dadosModelos, setDadosModelos] = useState();
+
+  const buscaModelos = async () => {
+    try {
+      const response = await axios
+        .get("http://localhost:3010/visualizar-modelos/")
+        .then((response) => {
+          setDadosModelos(response);
+        });
+
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <Body>
         <Menu />
         <DivContentCadastros>
           <NavegacaoCadastro />
-          <Listagem title="Modelos" />
+          <Listagem title="Modelos" rota="modelo" dadosModelos={dadosModelos} />
+          <button
+            type="submit"
+            onClick={() => {
+              buscaModelos();
+            }}
+          >
+            teste
+          </button>
         </DivContentCadastros>
       </Body>
     </>
