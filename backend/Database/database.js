c75e7ff7972af3aac862f87ce6e97849fcc2592f
fernando-bertolo <fernando.bertolo.jr123@gmail.com>
2024-04-09 17:50:51 -0300

    fix: ajustando bug no docker

--- backend/Database/database.js
+++ backend/Database/database.js
@@ -2,18 +2,18 @@
 
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize("databasecar", "root", "root", {
-    dialect: "mysql", // o banco que vai utilizar e o sequelize traduzir o js
-    host: "projeto-tcc-database-1", // host é onde esta o banco
-    port: "3306", // porta do banco
-    logging: console.log
+  dialect: "mysql", // o banco que vai utilizar e o sequelize traduzir o js
+  host: "database", // host é onde esta o banco
+  port: "3030", // porta do banco
+  logging: console.log,
 });
 
 sequelize
-    .authenticate()
-    .then(function () {
-        console.log("Conectado com sucesso");
-    })
-    .catch(function (erro){
-        console.log(`Falha ao se conectar: ${erro}`);
-    })
+  .authenticate()
+  .then(function () {
+    console.log("Conectado com sucesso");
+  })
+  .catch(function (erro) {
+    console.log(`Falha ao se conectar: ${erro}`);
+  });
 module.exports = sequelize;
