fc90c7826d066c116a729fb0303a5de6785fc9f7
fernando-bertolo <fernando.bertolo.jr123@gmail.com>
2024-04-25 22:06:52 -0300

    feat: configurando smtp do google para envio de e-mail

--- backend/api/Services/Email/mail.js
+++ backend/api/Services/Email/mail.js
@@ -1,11 +1,13 @@
 const nodemailer = require("nodemailer");
 
 const smtp = nodemailer.createTransport({
-  host: "sandbox.smtp.mailtrap.io",
-  port: 2525,
+  service: "Gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
-    user: "0c0b31e0ae45f6",
-    pass: "bb16b88bf34e27",
+    user: "fernando.bertolo.jr123@gmail.com",
+    pass: "yetq spel hfwe fflt",
   },
 });
 
