a98b619135888f69d6d57599b393392286a6acdb
fernando-bertolo <fernando.bertolo.jr123@gmail.com>
2024-04-26 21:13:09 -0300

    testando algumas coisas

--- backend/api/Services/ResetarSenha/resetarSenha.js
+++ backend/api/Services/ResetarSenha/resetarSenha.js
@@ -4,59 +4,60 @@ const tabelaUsuario = require("../../../Database/Tabelas/Usuarios/usuarios.js");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
-
-
 resetarSenha.post("/resetar-senha/:tokenSenha", async (request, response) => {
-    const {tokenSenha} = request.params;
-    const {senha, confirmaSenha} = request.body; // O confirmaSenha esta pegando o input no body
+  const { tokenSenha } = request.params;
+  const { senha, confirmaSenha } = request.body; // O confirmaSenha esta pegando o input no body
 
-    try {
+  try {
+    const usuario = await tabelaUsuario.findOne({
+      where: {
+        tokenSenha: tokenSenha, // Procura no banco quando tokenSenha da requisição for igual ao tokenSenha do banco
+      },
+    });
 
-        const usuario = await tabelaUsuario.findOne({
-            where: {
-                tokenSenha: tokenSenha // Procura no banco quando tokenSenha da requisição for igual ao tokenSenha do banco
-            }
-        })
-
-        //Se a variavel usuario não for true entra no IF
-        if(!usuario){
-            return response.status(400).send({error: "Token inválido"})
-        }
-
-        if(senha === "" && confirmaSenha === ""){
-            return response.status(400).send({error: "O campo de senha deve ser preenchido"});
-
-        } else if(senha !== confirmaSenha){
-            return response.status(400).send({error: "Senhas diferentes!"});
-
-        } else if(senha.length < 5 && confirmaSenha.length < 5){
-            return response.status(400).send({error: "A senha deverá ter no mínimo 5 caracteres"});
-        } else{
-            
-            //Pega a hora atual
-            const horaAtual = new Date();
-
-            if(horaAtual > usuario.horaExpiracaoToken){
-                return response.status(400).send({error: "Token expirado"})
-            }
-
-        
-            //Realizando o hash da senha novamente
-            const hashSenha = bcrypt.hashSync(request.body.senha, saltRounds);
-
-
-            await tabelaUsuario.update({
-                senha: hashSenha, //Altera o campo senha no banco com o hashSenha
-            },
-            {where: {tokenSenha: request.params.tokenSenha}}) // Realiza a alteração somente quando o tokenSenha no banco for igual ao tokenSenha da requisição
-
-            response.send({message: "Senha alterada com sucesso!!"});
-            }
-        
-    } catch (error) {
-        console.log(error);
-        return response.status(400).send({ error: "Não foi possível resetar a senha, tente novamente!"})
+    //Se a variavel usuario não for true entra no IF
+    if (!usuario) {
+      return response.status(400).send({ error: "Token inválido" });
     }
-})
+
+    if (senha === "" && confirmaSenha === "") {
+      return response
+        .status(400)
+        .send({ error: "O campo de senha deve ser preenchido" });
+    } else if (senha !== confirmaSenha) {
+      return response.status(400).send({ error: "Senhas diferentes!" });
+    } else if (senha.length < 5 && confirmaSenha.length < 5) {
+      return response
+        .status(400)
+        .send({ error: "A senha deverá ter no mínimo 5 caracteres" });
+    } else {
+      //Pega a hora atual
+      const horaAtual = new Date();
+
+      if (horaAtual > usuario.horaExpiracaoToken) {
+        return response.status(400).send({ error: "Token expirado" });
+      }
+
+      //Realizando o hash da senha novamente
+      const hashSenha = bcrypt.hashSync(request.body.senha, saltRounds);
+
+      const teste = await tabelaUsuario.update(
+        {
+          senha: hashSenha, //Altera o campo senha no banco com o hashSenha
+        },
+        { where: { tokenSenha: request.params.tokenSenha } }
+      ); // Realiza a alteração somente quando o tokenSenha no banco for igual ao tokenSenha da requisição
+
+      console.log(teste);
+
+      response.send({ message: "Senha alterada com sucesso!!" });
+    }
+  } catch (error) {
+    console.log(error);
+    return response
+      .status(400)
+      .send({ error: "Não foi possível resetar a senha, tente novamente!" });
+  }
+});
 
 module.exports = resetarSenha;
