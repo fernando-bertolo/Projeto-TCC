3ac4b902dbe08e68d892e2c2c289afd905769425
fernando-bertolo <fernando.bertolo.jr123@gmail.com>
2024-04-29 14:15:02 -0300

    Testando ZOD

--- frontend/src/Components/DialogMUI/DialogMUI.jsx
+++ frontend/src/Components/DialogMUI/DialogMUI.jsx
@@ -1,27 +1,43 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import { styled } from '@mui/material/styles';
-import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
-import DialogActions from '@mui/material/DialogActions';
-import IconButton from '@mui/material/IconButton';
-import CloseIcon from '@mui/material/Icon/Icon';
-import BotoesListagem from '../ButtonMUI/ButtonMUI';
-import InputsMUI from '../InputsMUI/InputsMUI';
+import * as React from "react";
+import Button from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/material/Icon/Icon";
+import BotoesListagem from "../ButtonMUI/ButtonMUI";
+import InputsMUI from "../InputsMUI/InputsMUI";
 //import AlertMUI from '../AlertMUI/AlertMUI';
-import axios from 'axios';
+import axios from "axios";
 import validator from "validator";
 import { ToastContainer, toast } from "react-toastify";
+import { z } from "zod";
 
-
-
+// Defina o esquema zod para validar os campos do formulário
+const schemaZod = z.object({
+  nome: z
+    .string()
+    .min(3, { message: "O nome deve ter no mínimo 3 caracteres" })
+    .max(50),
+  email: z.string().email({ message: "Insira um e-mail válido" }),
+  usuario: z.string().min(3, { message: "Insira um usuário válido" }).max(20),
+  senha: z
+    .string()
+    .min(5, { message: "A senha deve ter no mínimo 5 caracteres" })
+    .max(20),
+  confirmaSenha: z
+    .string()
+    .min(5, { message: "A senha deve ter no mínimo 5 caracteres" })
+    .max(20),
+});
 
 const BootstrapDialog = styled(Dialog)(({ theme }) => ({
-  '& .MuiDialogContent-root': {
+  "& .MuiDialogContent-root": {
     padding: theme.spacing(2),
   },
-  '& .MuiDialogActions-root': {
+  "& .MuiDialogActions-root": {
     padding: theme.spacing(1),
   },
 }));
@@ -41,57 +57,84 @@ function ModalDialog() {
     email: "",
     usuario: "",
     senha: "",
-    confirmaSenha: ""
+    confirmaSenha: "",
   });
-
 
   const SendUserData = async (event) => {
     event.preventDefault();
     try {
-        // Enviando os dados para a rota /criacao-usuario
-        await axios.post("http://localhost:3010/criacao-usuario", {
-          nome: userData.nome,
-          email: userData.email,
-          usuario: userData.usuario,
-          senha: userData.senha,
-          confirmaSenha: userData.confirmaSenha,
-        });
+      const userDataValidate = schemaZod.parse({
+        nome: userData.nome,
+        email: userData.email,
+        usuario: userData.usuario,
+        senha: userData.senha,
+        confirmaSenha: userData.confirmaSenha,
+      });
 
-        // Mensagem de sucesso
-        toast.success("Usuario criado com sucesso", {
-          position: "bottom-right",
-          autoClose: 2000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "light",
-        });
-      
-      
+      console.log(userDataValidate);
+
+      // Enviando os dados para a rota /criacao-usuario
+      await axios.post("http://localhost:3010/criacao-usuario", {
+        nome: userData.nome,
+        email: userData.email,
+        usuario: userData.usuario,
+        senha: userData.senha,
+        confirmaSenha: userData.confirmaSenha,
+      });
+
+      // Mensagem de sucesso
+      toast.success("Usuario criado com sucesso", {
+        position: "bottom-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
     } catch (error) {
-      
-    }
-  }
+      // Mensagem de aviso
+      toast.warn(error.errors[0].message, {
+        position: "bottom-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
 
+      console.log(error);
+    }
+  };
 
   return (
     <React.Fragment>
-      <BotoesListagem handleClickOpen={handleClickOpen}/>
+      <BotoesListagem handleClickOpen={handleClickOpen} />
       <BootstrapDialog
         onClose={handleClose}
         aria-labelledby="customized-dialog-title"
         open={open}
       >
-        <DialogTitle sx={{ m: 0, p: 2, display: "flex", justifyContent: "center", alignItems: "center" }} id="customized-dialog-title">
+        <DialogTitle
+          sx={{
+            m: 0,
+            p: 2,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          id="customized-dialog-title"
+        >
           Cadastro de Usuário
         </DialogTitle>
         <IconButton
           aria-label="close"
           onClick={handleClose}
           sx={{
-            position: 'absolute',
+            position: "absolute",
             right: 8,
             top: 8,
             color: (theme) => theme.palette.grey[500],
@@ -100,18 +143,21 @@ function ModalDialog() {
           <CloseIcon />
         </IconButton>
         <DialogContent dividers>
-        <InputsMUI setUserData={setUserData} userData={userData} SendUserData={SendUserData}></InputsMUI>
+          <InputsMUI setUserData={setUserData} userData={userData}></InputsMUI>
         </DialogContent>
         <DialogActions>
-          <Button type='submit' autoFocus onClick={(event) => SendUserData(event)}>
+          <Button
+            type="submit"
+            autoFocus
+            onClick={(event) => SendUserData(event)}
+          >
             Salvar
           </Button>
         </DialogActions>
       </BootstrapDialog>
-      <ToastContainer/>
+      <ToastContainer />
     </React.Fragment>
   );
 }
-
 
 export default ModalDialog;
--- frontend/src/Components/InputsMUI/InputsMUI.jsx
+++ frontend/src/Components/InputsMUI/InputsMUI.jsx
@@ -1,10 +1,9 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import FormControl from '@mui/material/FormControl';
-import Input from '@mui/material/Input';
-import InputLabel from '@mui/material/InputLabel';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import FormControl from "@mui/material/FormControl";
+import Input from "@mui/material/Input";
+import InputLabel from "@mui/material/InputLabel";
 import { z } from "zod";
-
 
 // Defina o esquema zod para validar os campos do formulário
 const schema = z.object({
@@ -12,123 +11,93 @@ const schema = z.object({
   email: z.string().email(),
   usuario: z.string().min(3).max(20),
   senha: z.string().min(6).max(20),
-  confirmaSenha: z.string().min(6).max(20)
+  confirmaSenha: z.string().min(6).max(20),
 });
 
-
-
 export default function InputsMUI(props) {
-  
-  // Estado para armazenar os erros de validação
-  const [errors, setErrors] = React.useState({})
-
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     props.setUserData({
       ...props.userData,
-      [name]: value
+      [name]: value,
     });
   };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      // Valide os dados do formulário com base no esquema zod
-      schema.parse(props.userData);
-      // Se a validação for bem-sucedida, envie os dados para o backend
-      await props.SendUserData();
-    } catch (error) {
-      // Se a validação falhar, defina os erros no estado de erros
-      setErrors(error.errors.reduce((acc, curr) => {
-        return { ...acc, [curr.path[0]]: curr.message };
-      }, {}));
-    }
-  };
-
 
   return (
     <Box
       component="form"
       sx={{
-        '& > :not(style)': { m: 4 },
+        "& > :not(style)": { m: 4 },
         display: "flex",
         flexDirection: "column",
         justifyContent: "center",
-        alignItems: "center"
+        alignItems: "center",
       }}
       noValidate
       autoComplete="off"
-      onSubmit={handleSubmit}
-      
     >
       <FormControl variant="standard">
         <InputLabel htmlFor="component-simple">Nome</InputLabel>
-        <Input 
-            type='text' 
-            id="nome"
-            name="nome" 
-            placeholder='Insira o Nome'
-            required
-            value={props.userData.nome}
-            onChange={handleInputChange}
+        <Input
+          type="text"
+          id="nome"
+          name="nome"
+          placeholder="Insira o Nome"
+          required
+          value={props.userData.nome}
+          onChange={handleInputChange}
         />
-        {errors.nome && <div>{errors.nome}</div>} {/* Exibir erro, se houver */}
       </FormControl>
 
       <FormControl variant="standard">
         <InputLabel htmlFor="component-simple">E-mail</InputLabel>
-        <Input 
-            type='email' 
-            id="email"
-            name='email'
-            placeholder='Insira o E-mail'
-            required
-            value={props.userData.email}
-            onChange={handleInputChange}
+        <Input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Insira o E-mail"
+          required
+          value={props.userData.email}
+          onChange={handleInputChange}
         />
-        {errors.email && <div>{errors.email}</div>} {/* Exibir erro, se houver */}
       </FormControl>
 
       <FormControl variant="standard">
         <InputLabel htmlFor="component-simple">Usuário</InputLabel>
-        <Input 
-            type='text' 
-            id="usuario"
-            name='usuario'
-            placeholder='Insira o Usuário'
-            required
-            value={props.userData.usuario}
-            onChange={handleInputChange}
+        <Input
+          type="text"
+          id="usuario"
+          name="usuario"
+          placeholder="Insira o Usuário"
+          required
+          value={props.userData.usuario}
+          onChange={handleInputChange}
         />
-        {errors.usuario && <div>{errors.usuario}</div>} {/* Exibir erro, se houver */}
       </FormControl>
 
       <FormControl variant="standard">
         <InputLabel htmlFor="component-simple">Senha</InputLabel>
-        <Input 
-            type='password' 
-            id="senha"
-            name="senha" 
-            placeholder='Insira a Senha'
-            required
-            onChange={handleInputChange}
+        <Input
+          type="password"
+          id="senha"
+          name="senha"
+          placeholder="Insira a Senha"
+          required
+          onChange={handleInputChange}
         />
-        {errors.senha && <div>{errors.senha}</div>} {/* Exibir erro, se houver */}
       </FormControl>
 
       <FormControl variant="standard">
         <InputLabel htmlFor="component-simple">Confirme sua Senha</InputLabel>
-        <Input 
-            type='password'
-            id="confirmaSenha"
-            name="confirmaSenha"
-            placeholder='Confirme sua Senha' 
-            required
-            onChange={handleInputChange}
+        <Input
+          type="password"
+          id="confirmaSenha"
+          name="confirmaSenha"
+          placeholder="Confirme sua Senha"
+          required
+          onChange={handleInputChange}
         />
-        {errors.confirmaSenha && <div>{errors.confirmaSenha}</div>} {/* Exibir erro, se houver */}
       </FormControl>
-
     </Box>
   );
 }
